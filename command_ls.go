053c76026128b931e0b5a5bf49517accc2edfd3c
Ryota Fujii <ryot4@readonly.xyz>
2020-01-29 00:33:13 +0900

    Rename list to ls

--- command_ls.go
+++ command_ls.go
@@ -8,13 +8,13 @@ import (
 	"time"
 )
 
-func runList(args []string) {
+func runLs(args []string) {
 	var longFormat, sortByMtime bool
 
-	listFlag := flag.NewFlagSet("list", flag.ExitOnError)
-	listFlag.BoolVar(&longFormat, "l", false, "long format")
-	listFlag.BoolVar(&sortByMtime, "t", false, "sort by modification time")
-	listFlag.Parse(args)
+	lsFlag := flag.NewFlagSet("ls", flag.ExitOnError)
+	lsFlag.BoolVar(&longFormat, "l", false, "long format")
+	lsFlag.BoolVar(&sortByMtime, "t", false, "sort by modification time")
+	lsFlag.Parse(args)
 
 	dir, err := OpenDirectory(NotePath)
 	if err != nil {
@@ -22,7 +22,7 @@ func runList(args []string) {
 		os.Exit(1)
 	}
 
-	listPaths := listFlag.Args()
+	listPaths := lsFlag.Args()
 	if len(listPaths) == 0 {
 		listPaths = append(listPaths, "/")
 	}
