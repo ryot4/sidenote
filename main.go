c2a505b6fb9aa999cbfca2f1d1e3307b70b4cef5
Ryota Fujii <ryot4@readonly.xyz>
2020-02-01 13:34:09 +0900

    Find notes upward if not found

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const (
-	NotePath = ".notes"
+	NoteDir = ".notes"
 )
 
 type HandlerFunc func(notePath string, args []string)
@@ -19,9 +20,37 @@ var handlers = map[string]HandlerFunc{
 }
 
 func main() {
-	notePath := flag.String("d", NotePath, "path to the directory for notes")
+	notePath := flag.String("d", findNotePath(), "path to the directory for notes")
 	flag.Parse()
 	run(*notePath, flag.Args())
+}
+
+func findNotePath() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cannot get current directory: %s\n", err)
+		os.Exit(1)
+	}
+	separator := string(filepath.Separator)
+	for dir := wd; dir != "." && dir != separator; dir = filepath.Dir(dir) {
+		notePath := filepath.Join(dir, NoteDir)
+		fi, err := os.Stat(notePath)
+		if err != nil {
+			if !os.IsNotExist(err) {
+				fmt.Fprintf(os.Stderr, "cannot stat %s: %s. ignoring\n", notePath, err)
+			}
+			continue
+		}
+		if fi.IsDir() {
+			rel, err := filepath.Rel(wd, notePath)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "cannot get relative path to %s: %s. ignoring\n", notePath, err)
+				continue
+			}
+			return rel
+		}
+	}
+	return NoteDir
 }
 
 func run(notePath string, args []string) {
